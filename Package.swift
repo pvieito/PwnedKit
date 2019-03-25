2096f30f819a70fb7b427f3a352c674764d691db
Pedro José Pereira Vieito <pvieito@gmail.com>
2019-03-25 01:28:28 +0100

    Swift 5 + macOS Swift PM support

--- Package.swift
+++ Package.swift
@@ -5,16 +5,30 @@ import PackageDescription
 let package = Package(
     name: "PwnedKit",
     products: [
-        .executable(name: "PwnedTool", targets: ["PwnedTool"]),
-        .library(name: "PwnedKit", targets: ["PwnedKit"])
+        .executable(
+            name: "PwnedTool",
+            targets: ["PwnedTool"]
+        ),
+        .library(
+            name: "PwnedKit",
+            targets: ["PwnedKit"]
+        )
     ],
     dependencies: [
         .package(url: "https://github.com/pvieito/CommandLineKit.git", .branch("master")),
         .package(url: "https://github.com/pvieito/LoggerKit.git", .branch("master")),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .branch("master")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .branch("master"))
     ],
     targets: [
-        .target(name: "PwnedTool", dependencies: ["LoggerKit", "CommandLineKit", "PwnedKit"], path: "PwnedTool"),
-        .target(name: "PwnedKit", dependencies: ["CryptoSwift"], path: "PwnedKit"),
+        .target(
+            name: "PwnedTool",
+            dependencies: ["LoggerKit", "CommandLineKit", "PwnedKit"],
+            path: "PwnedTool"
+        ),
+        .target(
+            name: "PwnedKit",
+            dependencies: ["CryptoSwift"],
+            path: "PwnedKit"
+        )
     ]
 )
