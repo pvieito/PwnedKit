d5290e0ca7f5686c7fd345e051cdcc334a8f88a3
Pedro José Pereira Vieito <pvieito@gmail.com>
2021-07-03 00:24:13 +0200

    [Infrastructure] Updated package

--- Package.swift
+++ Package.swift
@@ -18,9 +18,9 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/pvieito/CommandLineKit.git", .branch("master")),
-        .package(url: "https://github.com/pvieito/FoundationKit.git", .branch("master")),
-        .package(url: "https://github.com/pvieito/LoggerKit.git", .branch("master")),
+        .package(url: "git@github.com:pvieito/CommandLineKit.git", .branch("master")),
+        .package(url: "git@github.com:pvieito/FoundationKit.git", .branch("master")),
+        .package(url: "git@github.com:pvieito/LoggerKit.git", .branch("master")),
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.6")),
         .package(url: "https://github.com/apple/swift-crypto", from: "1.0.0"),
     ],
